36b14e3c2a3dc7bbc628bfbeab0f4edb67240fda
Preetisam <preetikamilla@gmail.com>
2023-06-30 16:11:13 +0530

    mongoose

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+// Set the strictQuery option to prepare for the change in Mongoose 7
+mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+    console.log(`MongoDB connected: ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
-    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+    console.log(`MongoDB server issue: ${error}`.bgRed.white);
   }
 };
 
